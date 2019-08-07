ee83574687a3438225a4b125b12f58284743bb7d
Joseph Escobedo <joe_escob1023@live.com>
2019-08-07 14:05:13 -0700

    I left a console.log in the App.js file, checking the length, it first loads 0 and then loads 6, i think this has something to do with the prop key not working right

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Header from "./components/Header";
 import Puppies from "./components/Puppies";
 import axios from "axios";
 
+//look in the console, its loading twice and the first length is 0 and the next is 6
+//I think this has something to do with the key prop not acting right
+
 export class App extends Component {
   state = {
     puppies: []
@@ -15,6 +18,7 @@ export class App extends Component {
   }
 
   render() {
+    console.log(this.state.puppies.length);
     return (
       <div>
         <Header />
